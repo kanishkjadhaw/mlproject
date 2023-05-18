3926345d37535e0b9f3fdb731f4da2aaa3c61cd6
KANISHK JADHAW <kaniahkjadhaw@gmail.com>
2023-05-18 23:43:26 +0530

    Eda and Problem statement

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup  # Import necessary functions from setuptools module
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)-->List(str):
-    '''
-    this function will return the list of requirements
-    '''
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+HYPEN_E_DOT = '-e .'  # Define a constant variable
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function retrieves the requirements from a file and returns them as a list.
+    '''
+    requirements = []  # Initialize an empty list to store requirements
+    with open(file_path) as file_obj:  # Open the specified file
+        requirements = file_obj.readlines()  # Read all lines from the file
+        requirements = [req.replace('\n', '') for req in requirements]  # Remove newline characters
 
-    return requirements            
+        if HYPEN_E_DOT in requirements:  # Check if '-e .' is present in requirements
+            requirements.remove(HYPEN_E_DOT)  # Remove '-e .' from the list
+    return requirements  # Return the list of requirements
 
 
 setup(
-name='mlproject',
-version='0.0.1',
-author='kanishk',
-author_email='kaniahkjadhaw@gmail.com',
-packages=find_packages(),
-#install_requires=['pandas','numpy','seaborn']
-install_requires=get_requirements('requirement.txt')
-)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    name='Student',  # Specify the name of the package
+    version='0.0.1',  # Specify the version of the package
+    author='kanishk_jadhaw',  # Specify the author of the package
+    author_email='kaniahkjadhaw@gmail.com',  # Specify the author's email
+    packages=find_packages(),  # Automatically find all packages and modules in the current directory
+    install_requires=get_requirements('requirements.txt'),  # Retrieve requirements from 'requirements.txt' file
+)
